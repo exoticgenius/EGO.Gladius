ad894f32e5b119a7cc6183c8327d7d37e9cb4187
farshad sadeghi <Exoticgenius@outlook.com>
2025-10-01 16:22:22 +0330

    new extensions

--- EGO.Gladius/Extensions/MigratorExtensions.cs
+++ EGO.Gladius/Extensions/MigratorExtensions.cs
@@ -108,3 +108,200 @@ public static class MigratorExtensions_DSPR_TO_TDSPR
     public static ValueTask<TDSPR<T>> MarkScope<T, E>(this ValueTask<DSPR<T>> taskSpr, E index) where E : Enum =>
         MarkScope(taskSpr, Convert.ToInt16(index));
 }
+
+public static class MigratorExtensions_DSPR_TO_DSPR
+{
+    public static async ValueTask<DSPR<T>> MarkDispose<T>(this Task<DSPR<T>> taskSpr, short index = -1)
+    {
+        DSPR<T> spr = await taskSpr;
+        try
+        {
+            if (spr.Succeed())
+                return spr.MarkDispose(index);
+
+            return spr;
+        }
+        catch (Exception e)
+        {
+            return spr.Pass<T>(SPF.Gen([spr.Value.Payload], e));
+        }
+    }
+    public static ValueTask<DSPR<T>> MarkDispose<T, E>(this Task<DSPR<T>> taskSpr, E index) where E : Enum =>
+        MarkDispose(taskSpr, Convert.ToInt16(index));
+
+    [OverloadResolutionPriority(1)]
+    public static async ValueTask<DSPR<T>> MarkDispose<T>(this ValueTask<DSPR<T>> taskSpr, short index = -1)
+    {
+        DSPR<T> spr = await taskSpr;
+        try
+        {
+            if (spr.Succeed())
+                return spr.MarkDispose(index);
+
+            return spr;
+        }
+        catch (Exception e)
+        {
+            return spr.Pass<T>(SPF.Gen([spr.Value.Payload], e));
+        }
+    }
+    [OverloadResolutionPriority(1)]
+    public static ValueTask<DSPR<T>> MarkDispose<T, E>(this ValueTask<DSPR<T>> taskSpr, E index) where E : Enum =>
+        MarkDispose(taskSpr, Convert.ToInt16(index));
+
+
+    public static async ValueTask<DSPR<T>> Dispose<T>(this Task<DSPR<T>> taskSpr, short index = -1)
+    {
+        DSPR<T> spr = await taskSpr;
+        try
+        {
+            if (spr.Succeed())
+                return spr.Dispose(index);
+
+            return spr;
+        }
+        catch (Exception e)
+        {
+            return spr.Pass<T>(SPF.Gen([spr.Value.Payload], e));
+        }
+    }
+    public static ValueTask<DSPR<T>> Dispose<T, E>(this Task<DSPR<T>> taskSpr, E index) where E : Enum =>
+        Dispose(taskSpr, Convert.ToInt16(index));
+
+    [OverloadResolutionPriority(1)]
+    public static async ValueTask<DSPR<T>> Dispose<T>(this ValueTask<DSPR<T>> taskSpr, short index = -1)
+    {
+        DSPR<T> spr = await taskSpr;
+        try
+        {
+            if (spr.Succeed())
+                return spr.Dispose(index);
+
+            return spr;
+        }
+        catch (Exception e)
+        {
+            return spr.Pass<T>(SPF.Gen([spr.Value.Payload], e));
+        }
+    }
+    [OverloadResolutionPriority(1)]
+    public static ValueTask<DSPR<T>> Dispose<T, E>(this ValueTask<DSPR<T>> taskSpr, E index) where E : Enum =>
+        Dispose(taskSpr, Convert.ToInt16(index));
+
+
+    public static async ValueTask<DSPR<T>> DisposeAsync<T>(this Task<DSPR<T>> taskSpr, short index = -1)
+    {
+        DSPR<T> spr = await taskSpr;
+        try
+        {
+            if (spr.Succeed())
+                return await spr.DisposeAsync(index);
+
+            return spr;
+        }
+        catch (Exception e)
+        {
+            return spr.Pass<T>(SPF.Gen([spr.Value.Payload], e));
+        }
+    }
+    public static ValueTask<DSPR<T>> DisposeAsync<T, E>(this Task<DSPR<T>> taskSpr, E index) where E : Enum =>
+        DisposeAsync(taskSpr, Convert.ToInt16(index));
+
+    [OverloadResolutionPriority(1)]
+    public static async ValueTask<DSPR<T>> DisposeAsync<T>(this ValueTask<DSPR<T>> taskSpr, short index = -1)
+    {
+        DSPR<T> spr = await taskSpr;
+        try
+        {
+            if (spr.Succeed())
+                return await spr.DisposeAsync(index);
+
+            return spr;
+        }
+        catch (Exception e)
+        {
+            return spr.Pass<T>(SPF.Gen([spr.Value.Payload], e));
+        }
+    }
+    [OverloadResolutionPriority(1)]
+    public static ValueTask<DSPR<T>> DisposeAsync<T, E>(this ValueTask<DSPR<T>> taskSpr, E index) where E : Enum =>
+        DisposeAsync(taskSpr, Convert.ToInt16(index));
+
+
+    public static async ValueTask<SPR<T>> DisposeAll<T>(this Task<DSPR<T>> taskSpr)
+    {
+        DSPR<T> spr = await taskSpr;
+        try
+        {
+            if (spr.Succeed())
+                return spr.DisposeAll();
+
+            return spr.Fault;
+        }
+        catch (Exception e)
+        {
+            return SPF.Gen([spr.Value.Payload], e);
+        }
+    }
+    public static ValueTask<SPR<T>> DisposeAll<T, E>(this Task<DSPR<T>> taskSpr) =>
+        DisposeAll(taskSpr);
+
+    [OverloadResolutionPriority(1)]
+    public static async ValueTask<SPR<T>> DisposeAll<T>(this ValueTask<DSPR<T>> taskSpr)
+    {
+        DSPR<T> spr = await taskSpr;
+        try
+        {
+            if (spr.Succeed())
+                return spr.DisposeAll();
+
+            return spr.Fault;
+        }
+        catch (Exception e)
+        {
+            return SPF.Gen([spr.Value.Payload], e);
+        }
+    }
+    [OverloadResolutionPriority(1)]
+    public static ValueTask<SPR<T>> DisposeAll<T, E>(this ValueTask<DSPR<T>> taskSpr) =>
+        DisposeAll(taskSpr);
+
+
+    public static async ValueTask<SPR<T>> DisposeAllAsync<T>(this Task<DSPR<T>> taskSpr)
+    {
+        DSPR<T> spr = await taskSpr;
+        try
+        {
+            if (spr.Succeed())
+                return await spr.DisposeAllAsync();
+
+            return spr.Fault;
+        }
+        catch (Exception e)
+        {
+            return SPF.Gen([spr.Value.Payload], e);
+        }
+    }
+    public static ValueTask<SPR<T>> DisposeAllAsync<T, E>(this Task<DSPR<T>> taskSpr) =>
+        DisposeAllAsync(taskSpr);
+
+    [OverloadResolutionPriority(1)]
+    public static async ValueTask<SPR<T>> DisposeAllAsync<T>(this ValueTask<DSPR<T>> taskSpr)
+    {
+        DSPR<T> spr = await taskSpr;
+        try
+        {
+            if (spr.Succeed())
+                return await spr.DisposeAllAsync();
+
+            return spr.Fault;
+        }
+        catch (Exception e)
+        {
+            return SPF.Gen([spr.Value.Payload], e);
+        }
+    }
+    [OverloadResolutionPriority(1)]
+    public static ValueTask<SPR<T>> DisposeAllAsync<T, E>(this ValueTask<DSPR<T>> taskSpr) =>
+        DisposeAllAsync(taskSpr);
+}
